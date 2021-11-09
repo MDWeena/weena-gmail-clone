ae2bde62934899dd01e8cb239f1f5bfc5aa98cb9
unknown <akpologun.winner@gmail.com>
2021-11-09 13:37:06 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header'
+import './App.css';
+import SideBar from './SideBar';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
